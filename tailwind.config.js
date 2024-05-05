12c9410e95f467c1518e75042e13b947252d4f20
KrushanthAmalanathan <159056340+KrushanthAmalanathan@users.noreply.github.com>
2024-05-05 11:32:46 +0530

    Start Front

---
+++ tailwind.config.js
@@ -0,0 +1,93 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      Lavish: ["Lavish", "sans-serif"],
+      Philosopher: ["Philosopher", "sans-serif"],
+      BreeSerif: ["BreeSerif", "sans-serif"]
+    },
+    extend: {
+      colors: {
+        //Maneth
+        'primary': '#da6e2e',
+        'secondary': '#c15026',
+        'ternary': '#6e2e2c',
+        'bgc': '#FCEADC',
+
+
+
+
+
+
+
+
+        //Sandithi
+
+
+
+
+
+
+
+
+
+
+        //Ridmi
+        'formbg' : "#D9D9D9",
+        'PMnavbar' : "#DEE2E6",
+        'RawmRequest' : "#F8D3C0"
+
+
+
+
+
+
+
+
+
+        //Gihan
+
+
+
+
+
+
+
+
+        //Sageevan
+
+
+
+
+
+
+
+
+        //Isuru
+
+
+
+
+
+
+
+
+
+        //Hiranya
+        
+
+
+
+
+
+
+      },
+    },
+  },
+  plugins: [],
+}
+
